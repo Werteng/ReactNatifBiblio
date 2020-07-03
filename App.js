9a80e7105933a7174cab2ba07290b56b0eedea2a
Werteng <antoine.werteng@gmail.com>
2020-07-03 16:50:32 +0200

    mise a jour

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const App = () => {
+  const [isConnected, setIsConnected] = useState(false)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [scanned, setScanned] = useState(false)
+
+  const handleConnection = () => {
+    if (username.length > 3 && password.length > 3)
+    setIsConnected(true)
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true)
+    alert(`Le code bare de type: ${type}. Avec comme données : ${data} a été scanné`)
+  }
+
+  if (isConnected) {
+    return (
+      <View style={styles.container}>
+        <Text>Vous pouvez scanner votre livre</Text>
+        <BarCodeScanner 
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Vous n'êtes pas connecté, veuillez vous connecter</Text>
+        <TextInput 
+          value={username}
+          placeholder="Nom d'utilisateur"
+          onChangeText={(value) => setUsername(value) }
+        />
+        <TextInput 
+          value={password}
+          secureTextEntry={true}
+          placeholder="Mot de passe"
+          onChangeText={(value) => setPassword(value) }
+        />
+        <Button 
+          title='Connexion'
+          onPress={handleConnection}
+        />
+      </View>      
+    )
+  }
+
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +64,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
